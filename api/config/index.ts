fe4ca100753237f3611fc3e34144dfa70df82a5e
James Hoff <jameshoff@microsoft.com>
2022-03-23 07:48:56 -0400

    update

--- api/config/index.ts
+++ api/config/index.ts
@@ -2,18 +2,27 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import { CosmosClient } from "@azure/cosmos";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const client = new CosmosClient(process.env.COSMOS_DB_CONNECTION_STRING);
-
-    const database = client.database(process.env.COSMOS_DB_DB);
-    const container = database.container(process.env.COSMOS_DB_CONTAINER);
-    const body = req.body
-    body.id = "1"
-    const { resource: createdItem } = await container.items.create(req.body);
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: createdItem
-    };
+    try{
+        context.log('HTTP trigger function processed a request.');
+        const client = new CosmosClient(process.env.COSMOS_DB_CONNECTION_STRING);
+    
+        const database = client.database(process.env.COSMOS_DB_DB);
+        const container = database.container(process.env.COSMOS_DB_CONTAINER);
+        const body = req.body
+        body.id = "1"
+        const { resource: createdItem } = await container.items.create(req.body);
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: createdItem
+        };
+    } catch(err){
+        context.log(err)
+        context.res = {
+            body : err
+        }
+    }
+    
+    
 };
 
 export default httpTrigger;
