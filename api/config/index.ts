7f3d9f6c3044ce6020f5d28af20bc8803e591cde
James Hoff <jameshoff@microsoft.com>
2022-03-23 13:00:06 -0400

    update

--- api/config/index.ts
+++ api/config/index.ts
@@ -3,26 +3,30 @@ import { CosmosClient } from "@azure/cosmos";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     try {
-        context.log('HTTP trigger function processed a request.');
-        const client = new CosmosClient(process.env.COSMOS_DB_CONNECTION_STRING);
-        context.log(`body : ${JSON.stringify(req.body)}`)
+        // context.log('HTTP trigger function processed a request.');
+        // const client = new CosmosClient(process.env.COSMOS_DB_CONNECTION_STRING);
+        // context.log(`body : ${JSON.stringify(req.body)}`)
 
-        const database = client.database(process.env.COSMOS_DB_DB);
-        const container = database.container(process.env.COSMOS_DB_CONTAINER);
-        const item = await container.item("1")
-        await item.delete()
-        
+        // const database = client.database(process.env.COSMOS_DB_DB);
+        // const container = database.container(process.env.COSMOS_DB_CONTAINER);
+        // await container.item(id, category).delete();
+        // const item = await container.item("1")
+        // await item.delete()
+        const out = create(context, req)
+        context.res = {
+            body: out
+        }
     } catch (err) {
         context.log(err)
         context.res = {
             body: err
         }
     } finally {
-        const out = await create(context, req)
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: out
-        };
+        // const out = await create(context, req)
+        // context.res = {
+        //     // status: 200, /* Defaults to 200 */
+        //     body: out
+        // };
     }
 
 
