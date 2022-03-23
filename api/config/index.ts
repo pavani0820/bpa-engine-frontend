e4c5cf571ad00b875fb8fabd649d20e709949442
James Hoff <jameshoff@microsoft.com>
2022-03-23 14:40:14 -0400

    update

--- api/config/index.ts
+++ api/config/index.ts
@@ -36,13 +36,10 @@ const create = async function (context: Context, req: HttpRequest): Promise<void
 
     context.log('HTTP trigger function processed a request.');
     const client = new CosmosClient(process.env.COSMOS_DB_CONNECTION_STRING);
-    context.log(`body : ${JSON.stringify(req.body)}`)
 
     const database = client.database(process.env.COSMOS_DB_DB);
     const container = database.container(process.env.COSMOS_DB_CONTAINER);
-    const newBody = req.body
-    container.delete()
-    const { resource: createdItem } = await container.items.create(newBody);
+    const { resource: createdItem } = await container.items.create(req.body);
     return createdItem
 
 };
