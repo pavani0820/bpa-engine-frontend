d864b5743669e4421c8a232b594412ea24215213
James Hoff <jameshoff@microsoft.com>
2022-03-29 12:56:42 -0400

    update

--- src/CurrentPipeline.jsx
+++ src/CurrentPipeline.jsx
@@ -1,18 +1,60 @@
+import axios from "axios"
 import { useEffect, useState } from "react"
+import OptionCard from "./OptionCard"
+import arrow from './images/arrow.png'
+
 
 export default function Stages() {
 
-    const [stages] = useState(null)
+    const [stages, setStages] = useState()
 
-    useEffect(()=> {
+    useEffect(() => {
+        try {
+            axios.get('/api/config').then(value => setStages(value.stages))
+        } catch (err) {
 
-    },[])
+        }
+
+    }, [])
 
 
-    if(stages){
+    const renderPipeline = () => {
+        if (stages) {
+            return (
+                <div style={{ display: "flex", padding: "30px" }} >
+                    {stages.map((option, index) => {
+                        console.log(`index : ${index}`)
+                        if (index === stages.length - 1) {
+                            return (
+                                <>
+                                    <OptionCard option={option} />
+                                </>)
+                        } else {
+                            return (
+                                <>
+                                    <OptionCard option={option} />
+                                    <img src={arrow} alt="progress indicator" />
+                                </>)
+                        }
+                    })}
+                </div>
+            )
+        }
+    }
+
+
+
+    if (stages) {
+        return (
+            <div style={{ display: "flex" }}>
+                <div style={{ flexDirection: "column" }}>
+                    {renderPipeline()}
+                </div>
+            </div>
+        )
 
     } else {
-        return(
+        return (
             <>No Pipeline Configured</>
         )
     }
