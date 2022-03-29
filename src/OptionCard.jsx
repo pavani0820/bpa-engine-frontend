5d23f3c81c72feb6f8feb8877007343d33009215
James Hoff <jameshoff@microsoft.com>
2022-03-29 08:06:38 -0400

    update

--- src/OptionCard.jsx
+++ src/OptionCard.jsx
@@ -4,7 +4,7 @@ export default function OptionCard(props) {
         <div id={props.option.name + "_optioncard"} onClick={()=>props.onClickHandler(props.option)} style={{ height: "320px", width: "263px", margin: "20px", borderStyle: "solid" }}>
             <img src={props.option.image} alt="top"/>
             <div style={{padding : "10px"}}>
-                {props.option.name}
+                {props.option.label}
             </div>
         </div>
     )
