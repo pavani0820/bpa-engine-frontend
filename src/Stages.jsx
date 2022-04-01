711758441ff6bbd6d301a255600bdac4f2ab9b88
James Hoff <jameshoff@microsoft.com>
2022-04-01 13:40:03 -0400

    update

--- src/Stages.jsx
+++ src/Stages.jsx
@@ -7,19 +7,9 @@ import _ from 'lodash'
 import { sc } from './serviceCatalog'
 import { PrimaryButton } from '@fluentui/react';
 import { Label } from '@fluentui/react/lib/Label';
-import { Dialog, DialogType, DialogFooter } from '@fluentui/react/lib/Dialog';
-import { Dropdown } from '@fluentui/react/lib/Dropdown';
+import LanguageDialog from './LanguageDialog';
+import FormRecCustomDialog from './FormRecCustomDialog';
 
-const dropdownStyles = {
-    dropdown: { width: 300 },
-};
-
-const languages = [
-    { key: 'en', text: 'English' },
-    { key: 'es', text: 'Spanish' },
-    { key: 'th', text: 'Thai' },
-    { key: 'fr', text: 'French' },
-];
 
 export default function Stages(props) {
 
@@ -28,7 +18,8 @@ export default function Stages(props) {
     const [value, setValue] = useState(0)
     const [options, setOptions] = useState([])
     const [hideTranslateDialog, setHideTranslateDialog] = useState(true)
-    const [selectedLanguage, setSelectedLanguage] = useState(null)
+    const [hideFormRecDialog, setHideFormRecDialog] = useState(true)
+
     const [currentOption, setCurrentOption] = useState(null)
 
 
@@ -47,13 +38,13 @@ export default function Stages(props) {
     }, [])
 
     const onDone = async () => {
-        try{
-            await axios.post('/api/config', { stages: stages.slice(1, stages.length), id: "1" })  
-        } catch(err) {
+        try {
+            await axios.post('/api/config', { stages: stages.slice(1, stages.length), id: "1" })
+        } catch (err) {
             console.log(err)
         }
 
-        props.onSelectContent({currentTarget : {id : "CURRENT_PIPELINE"}})
+        props.onSelectContent({ currentTarget: { id: "CURRENT_PIPELINE" } })
         setOptions([])
     }
 
@@ -99,9 +90,14 @@ export default function Stages(props) {
 
 
     const onItemClick = (event) => {
+        console.log(event.name)
         if (event.name === 'translate') {
             setCurrentOption(_.cloneDeep(event))
             setHideTranslateDialog(false)
+        } else if (event.name === 'customFormRec'){
+            console.log("here")
+            setCurrentOption(_.cloneDeep(event))
+            setHideFormRecDialog(false)
         } else {
             addItemToPipeline(event)
         }
@@ -118,72 +114,13 @@ export default function Stages(props) {
                 </div>
             )
         }
-
-    }
-
-    const toggleHideDialog = () => {
-        setHideTranslateDialog(!hideTranslateDialog)
-    }
-
-    const dialogContentProps = {
-        type: DialogType.largeHeader,
-        title: 'Translate To Language',
-        subText: 'Select the target language to translate your documents.',
-    };
-
-    const modalProps = {
-        isBlocking: false,
-        styles: { main: { maxWidth: 450 } },
-    };
-
-    const onDialogSave = (event) => {
-        console.log(event)
-        const newOption = currentOption
-        newOption.serviceSpecificConfig = { to: selectedLanguage }
-        setHideTranslateDialog(true)
-        addItemToPipeline(newOption)
-    }
-
-    const onDialogCancel = (event) => {
-        setHideTranslateDialog(true)
-    }
-
-    const onTranslateDialogChange = (event) => {
-        console.log(event)
-        let key = null
-        for (const l of languages) {
-            if (l.text === event.currentTarget.textContent) {
-                key = l.key
-                break
-            }
-        }
-        if (key) {
-            setSelectedLanguage(key)
-        }
-
     }
 
     const renderStageTop = () => {
         return (
             <>
-                <Dialog
-                    hidden={hideTranslateDialog}
-                    onDismiss={toggleHideDialog}
-                    dialogContentProps={dialogContentProps}
-                    modalProps={modalProps}
-                >
-                    <Dropdown
-                        placeholder="Select an option"
-                        label="Languages"
-                        options={languages}
-                        styles={dropdownStyles}
-                        onChange={onTranslateDialogChange}
-                    />
-                    <DialogFooter>
-                        <PrimaryButton onClick={onDialogSave} text="Save" />
-                        <PrimaryButton onClick={onDialogCancel} text="Cancel" />
-                    </DialogFooter>
-                </Dialog>
+                <FormRecCustomDialog hideDialog={hideFormRecDialog} setHideDialog={setHideFormRecDialog} currentOption={currentOption} addItemToPipeline={addItemToPipeline} />
+                <LanguageDialog hideDialog={hideTranslateDialog} setHideDialog={setHideTranslateDialog} currentOption={currentOption} addItemToPipeline={addItemToPipeline} />
                 <Label theme={props.theme} style={{ fontFamily: props.theme.fonts.xxLarge.fontFamily, fontSize: props.theme.fonts.xxLarge.fontSize }}>Select a stage to add it to your pipeline configuration</Label>
                 {renderOptions(options)}
             </>
@@ -191,7 +128,7 @@ export default function Stages(props) {
     }
 
     const renderStageBottom = () => {
-        if(stages && stages.length > 0){
+        if (stages && stages.length > 0) {
             return (
                 <>
                     <Label theme={props.theme} style={{ fontFamily: props.theme.fonts.xxLarge.fontFamily, fontSize: props.theme.fonts.xxLarge.fontSize }}>Pipeline Preview</Label>
@@ -203,21 +140,12 @@ export default function Stages(props) {
                 </>
             )
         }
-        
     }
 
-    const renderStage = () => {
-        return (
-            <div style={{ paddingLeft: "10px", paddingTop: "50px" }}>
-                {renderStageTop()}
-                {renderStageBottom()}
-            </div>
-        )
-    }
-
-    return (<>
-        {renderStage()}
-    </>)
-
-
+    return (
+        <div style={{ paddingLeft: "10px", paddingTop: "50px" }}>
+            {renderStageTop()}
+            {renderStageBottom()}
+        </div>
+    )
 }
