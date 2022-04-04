51d5e9f1e912db29afab3c86abbd3802dddb366d
Harmke Alkemade <harmkealkemade@gmail.com>
2022-04-04 15:09:14 +0200

    UI provider added

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import './styles/update.css'
 import Header from './Header';
 import Content from './Content';
 import { getTheme } from '@fluentui/react';
+import { Provider, teamsTheme } from '@fluentui/react-northstar'
+
 
 const theme = getTheme()
 
 function App({ Component, pageProps }) {
   return (
-    <>
+    <Provider theme={teamsTheme}>
       <Header theme={theme}/>
       <Content theme={theme}/>
-    </>
+    </Provider>
   )
 }
 
